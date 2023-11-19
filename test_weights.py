e065b6e3241ae9e46a1d9be80114194b2183ad2d
Prithwish Dan <dan.prithwish@gmail.com>
2023-11-19 11:59:56 -0500

    Updated test_weights.py to use new model weights

--- test_weights.py
+++ test_weights.py
@@ -9,7 +9,7 @@ from catanatron_experimental.machine_learning.players.value import DEFAULT_WEIGH
 from itertools import product
 import pandas as pd
 
-NUM_GAMES_EACH_SETTING = 100
+NUM_GAMES_EACH_SETTING = 20
 ######## DataFrame for output data ##############################
 player_summary = pd.DataFrame()
 game_summary = pd.DataFrame()
@@ -20,24 +20,32 @@ def set_equal_weights(weights, val):
     return weights
 
 default_w_0 = set_equal_weights(DEFAULT_WEIGHTS.copy(), 0)
+### Fully Adversarial - all weights 0 except enemy production
 AlphaBetaFullAd_w = default_w_0.copy()
-AlphaBetaFullAd_w['enemy_production'] = -5e8
+AlphaBetaFullAd_w['enemy_production'] = -5e12
 
-AlphaBetaFullPr_w = default_w_0.copy()
-AlphaBetaFullPr_w['production'] = 5e8
+default_w_1 = DEFAULT_WEIGHTS.copy()
+### Fully Production - all weights same as default but 0 enemy production
+AlphaBetaFullPr_w = default_w_1.copy()
+AlphaBetaFullPr_w['enemy_production'] = 0
 
-default_w_1 = set_equal_weights(DEFAULT_WEIGHTS.copy(), 1e3)
+### Mostly Adversarial, Slightly Production - divide all weights by 3 and increase
+###                                           importance of enemy production
 AlphaBetaMixedAd_w = default_w_1.copy()
-AlphaBetaMixedAd_w['enemy_production'] = -5e8
+for k, v in AlphaBetaMixedAd_w.items():
+    AlphaBetaMixedAd_w[k] = v/3
+AlphaBetaMixedAd_w['enemy_production'] = -5e12
 
+### Mostly Production, Slightly Adversarial - default weights but enemy production 
+###                                           only slightly negative
 AlphaBetaMixedPr_w = default_w_1.copy()
-AlphaBetaMixedAd_w['production'] = 5e8
+AlphaBetaMixedPr_w['enemy_production'] = -1e4
 
 ######### Define the player we are interested in #################
-AlphaBetaFullAd = AlphaBetaPlayer(Color.RED)
-AlphaBetaMixedAd = AlphaBetaPlayer(Color.RED)
-AlphaBetaFullPr = AlphaBetaPlayer(Color.RED)
-AlphaBetaMixedPr = AlphaBetaPlayer(Color.RED)
+AlphaBetaFullAd = AlphaBetaPlayer(Color.RED, params=AlphaBetaFullAd_w, value_fn_builder_name="A")
+AlphaBetaMixedAd = AlphaBetaPlayer(Color.RED, params=AlphaBetaMixedAd_w, value_fn_builder_name="A")
+AlphaBetaFullPr = AlphaBetaPlayer(Color.RED, params=AlphaBetaFullPr_w, value_fn_builder_name="A")
+AlphaBetaMixedPr = AlphaBetaPlayer(Color.RED, params=AlphaBetaMixedPr_w, value_fn_builder_name="A")
 
 new_agents = {AlphaBetaFullAd: 'abFullAd', AlphaBetaMixedAd: 'abMixedAd', AlphaBetaFullPr: 'abFullPr', AlphaBetaMixedPr: 'abMixedPr'} 
 ######### Define the baseline players ############################
@@ -46,6 +54,7 @@ mcts = MCTSPlayer(Color.BLUE)
 Random = RandomPlayer(Color.WHITE)
 
 base_agents = {AlphaBeta: 'ab', mcts: 'mcts', Random: 'random'}
+base_agents = {AlphaBeta: 'ab'}
 
 ######## Define the set of players for each game ################
 num_comp = len(base_agents.keys()) * len(new_agents.keys())
@@ -76,8 +85,8 @@ for idx, (new_agent, base_agent) in enumerate(comp):
 # game_summary = pd.concat([game_summary, df_g], axis = 0)
 # player_summary = pd.concat([player_summary, df_p], axis = 0)
 
-game_summary.to_csv("/Users/sl984/Documents/Fall23/ORIE4350/catan/catanatron/results/game_test.csv")
-player_summary.to_csv("/Users/sl984/Documents/Fall23/ORIE4350/catan/catanatron/results/player_test.csv")
+game_summary.to_csv("./results/game_test.csv")
+player_summary.to_csv("./results/player_test.csv")
 
 
 
